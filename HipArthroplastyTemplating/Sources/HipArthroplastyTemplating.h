07fbfa33daf29a8ded61ab7bd3f20433b4446b5b
Alessandro Volz <alessandro.volz@gmail.com>
2016-09-28 15:06:37 +0200

    just reverting distribution specific stuff

--- HipArthroplastyTemplating/Sources/HipArthroplastyTemplating.h
+++ HipArthroplastyTemplating/Sources/HipArthroplastyTemplating.h
@@ -10,7 +10,7 @@
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
-#import <OsiriXAPI/PluginFilter.h>
+#import <OsiriX/PluginFilter.h>
 #pragma clang diagnostic pop
 
 @class ArthroplastyTemplatingWindowController, ArthroplastyTemplatingStepsController;
